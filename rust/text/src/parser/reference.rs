8e052a10697bed091bdaa284b01a26148b4090ee
cronosun <silvergate@gmail.com>
2019-05-16 11:56:26 +0200

    implemented anchors

---
+++ rust/text/src/parser/reference.rs
@@ -0,0 +1,31 @@
+use liquesco_core::schema::reference::TReference;
+use crate::parser::converter::Converter;
+use crate::parser::core::Context;
+use crate::parser::core::ParseError;
+use crate::parser::core::Parser;
+use crate::parser::value::TextValue;
+use liquesco_core::serialization::core::Serializer;
+use liquesco_core::serialization::uint::UInt32;
+
+pub struct PReference;
+
+impl Parser<'static> for PReference {
+    type T = TReference;
+
+    fn parse<'c, C>(
+        context: &mut C,
+        writer: &mut C::TWriter,
+        value: &TextValue,
+        _: &Self::T,
+    ) -> Result<(), ParseError>
+    where
+        C: Context<'c>,
+    {
+        C::TConverter::require_no_name(value)?;
+        let reference_as_text = C::TConverter::require_text(value.as_ref())?;
+        // TODO: Validate reference as text
+        let reference_as_u32 = context.present_anchor_info().reference(reference_as_text);
+        UInt32::serialize(writer, &reference_as_u32)?;
+        Result::Ok(())
+    }
+}
