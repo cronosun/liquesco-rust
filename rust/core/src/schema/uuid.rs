bb894c2bba02f8d6553121725088b03b4173742b
cronosun <silvergate@gmail.com>
2019-05-18 11:40:05 +0200

    some changes

---
+++ rust/core/src/schema/uuid.rs
@@ -0,0 +1,49 @@
+use crate::common::error::LqError;
+use crate::common::ine_range::I64IneRange;
+use crate::schema::core::Context;
+use crate::schema::core::Type;
+use crate::serialization::core::DeSerializer;
+use crate::serialization::sint::SInt64;
+use std::cmp::Ordering;
+use crate::common::range::LqRangeBounds;
+
+#[derive(new, Clone, Debug)]
+pub struct TSInt {
+    pub range: I64IneRange,
+}
+
+impl TSInt {
+    pub fn try_new(min: i64, max: i64) -> Result<Self, LqError> {
+        Result::Ok(TSInt::new(I64IneRange::try_new_msg(
+            "Signed integer range",
+            min,
+            max,
+        )?))
+    }
+}
+
+impl Type for TSInt {
+    fn validate<'c, C>(&self, context: &mut C) -> Result<(), LqError>
+    where
+        C: Context<'c>,
+    {
+        let int_value = SInt64::de_serialize(context.reader())?;
+        self.range
+            .require_within("Signed integer schema validation", &int_value)?;
+        Result::Ok(())
+    }
+
+    fn compare<'c, C>(
+        &self,
+        _: &C,
+        r1: &mut C::Reader,
+        r2: &mut C::Reader,
+    ) -> Result<Ordering, LqError>
+    where
+        C: Context<'c>,
+    {
+        let int1 = SInt64::de_serialize(r1)?;
+        let int2 = SInt64::de_serialize(r2)?;
+        Result::Ok(int1.cmp(&int2))
+    }
+}
